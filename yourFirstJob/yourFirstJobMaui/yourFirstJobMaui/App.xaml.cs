0a860917c47fe0eb2f7b47f6e2589ec9e8378203
Valeria Alfaro Quesada <valfaro2409@gmail.com>
2024-03-19 21:40:06 -0600

    subir proyecto api y back

---
+++ yourFirstJob/yourFirstJobMaui/yourFirstJobMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace yourFirstJobMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
